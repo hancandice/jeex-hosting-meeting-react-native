cfbe23955b12e471d07eca65d1fbc7c2525c64e5
Candice Han <jeeyoung.han82@gmail.com>
2020-10-01 18:09:36 +0900

    ADD login error screen

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -11,14 +11,35 @@ import {
 import colors from "../styles/colors";
 import InputField from "../components/form/InputField";
 import NextArrowButton from "../components/buttons/NextArrowButton";
+import Notification from "../components/Notification";
 
 export default class LogIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValid: false,
+    };
+    this.handleCloseNotification = this.handleCloseNotification.bind(this);
+  }
+
   handleNextButton() {
     alert("next button pressed");
   }
+
+  handleCloseNotification() {
+    this.setState({
+      formValid: true,
+    });
+  }
   render() {
+    const { formValid } = this.state;
+    const showNotification = formValid ? false : true;
+    const background = formValid ? colors.green01 : colors.darkOrange;
     return (
-      <KeyboardAvoidingView style={styles.wrapper} behavior="padding">
+      <KeyboardAvoidingView
+        style={[{ backgroundColor: background }, styles.wrapper]}
+        behavior="padding"
+      >
         <View style={styles.scrollViewWrapper}>
           <ScrollView style={styles.scrollView}>
             <Text style={styles.loginHeader}>Log In</Text>
@@ -46,6 +67,15 @@ export default class LogIn extends Component {
               handleNextButton={this.handleNextButton}
             ></NextArrowButton>
           </View>
+          <View>
+            <Notification
+              showNotification={showNotification}
+              handleCloseNotification={this.handleCloseNotification}
+              type="Error"
+              firstLine="Those credentials don't look right."
+              secondLine="Please try again."
+            />
+          </View>
         </View>
       </KeyboardAvoidingView>
     );
@@ -56,7 +86,6 @@ const styles = StyleSheet.create({
   wrapper: {
     display: "flex",
     flex: 1,
-    backgroundColor: colors.green01,
   },
   scrollViewWrapper: {
     marginTop: 70,
--- src/styles/colors/index.js
+++ src/styles/colors/index.js
@@ -3,4 +3,6 @@ export default {
   white: "#ffffff",
   green01: "#3D6647",
   pink01: "#F4F1F1",
+  darkOrange: "#C53925",
+  lightGrey: "#d8d8d8",
 };
