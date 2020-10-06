49e7038220f549b8211278055d90e63a0385a1fe
Candice Han <jeeyoung.han82@gmail.com>
2020-10-06 23:20:52 +0900

    UPDATE ForgotPassword.js

--- src/screens/ForgotPassword.js
+++ src/screens/ForgotPassword.js
@@ -12,6 +12,9 @@ export default class ForgotPassword extends Component {
       <KeyboardAvoidingView style={styles.wrapper} behavior="padding">
         <View style={styles.form}>
           <Text style={styles.forgotPasswordHeading}>Forgot Password ?</Text>
+          <Text style={styles.forgotPasswordSubheading}>
+            Enter your email to find your account.
+          </Text>
         </View>
       </KeyboardAvoidingView>
     );
@@ -27,10 +30,19 @@ const styles = StyleSheet.create({
   form: {
     marginTop: 90,
     paddingLeft: 20,
+    paddingRight: 20,
+    flex: 1,
   },
   forgotPasswordHeading: {
     fontSize: 28,
     color: colors.white,
     fontWeight: "300",
+  },
+  forgotPasswordSubheading: {
+    color: colors.white,
+    fontWeight: "600",
+    fontSize: 15,
+    marginTop: 10,
+    marginBottom: 60,
   },
 });
