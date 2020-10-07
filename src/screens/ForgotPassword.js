9e19c61e44b16f1a89215e1cb2d2bc2969aa849b
Candice Han <jeeyoung.han82@gmail.com>
2020-10-07 22:09:39 +0900

    ADD InputField in ForgotPassword.js

--- src/screens/ForgotPassword.js
+++ src/screens/ForgotPassword.js
@@ -7,6 +7,31 @@ import NextArrowButton from "../components/buttons/NextArrowButton";
 import Loader from "../components/Loader";
 
 export default class ForgotPassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValid: true,
+      validEmail: false,
+      emailAddress: "",
+      loadingVisible: false,
+    };
+  }
+
+  handleEmailChange(email) {
+    const emailCheckRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; //regex function
+    this.setState({ emailAddress: email });
+
+    if (!this.state.validEmail) {
+      if (emailCheckRegex.test(email)) {
+        this.setState({ validEmail: true });
+      }
+    } else {
+      if (!emailCheckRegex.test(email)) {
+        this.setState({ validEmail: false });
+      }
+    }
+  }
+
   render() {
     return (
       <KeyboardAvoidingView style={styles.wrapper} behavior="padding">
@@ -15,11 +40,25 @@ export default class ForgotPassword extends Component {
           <Text style={styles.forgotPasswordSubheading}>
             Enter your email to find your account.
           </Text>
+          <InputField
+            customStyle={{ marginBottom: 30 }}
+            textColor={colors.white}
+            labelText="EMAIL ADDRESS"
+            labelTextSize={14}
+            labelColor={colors.white}
+            borderBottomColor={colors.white}
+            inputType="email"
+            onChangeText={this.handleEmailChange}
+            autoFocus={true}
+            autoCapitalize={false}
+          />
         </View>
       </KeyboardAvoidingView>
     );
   }
 }
+
+// showCheckmark
 
 const styles = StyleSheet.create({
   wrapper: {
