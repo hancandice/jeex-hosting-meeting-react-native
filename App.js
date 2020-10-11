46bb68b77374bd42314765250ecd899697728f98
Candice Han <jeeyoung.han82@gmail.com>
2020-10-11 18:57:03 +0900

    SETUP Redux

--- App.js
+++ App.js
@@ -4,6 +4,6 @@ import LogIn from "./src/screens/LogIn";
 import ForgotPassword from "./src/screens/ForgotPassword";
 export default class App extends Component {
   render() {
-    return <ForgotPassword />;
+    return <LogIn />;
   }
 }
---
+++ src/redux/reducers/loggedOut.js
@@ -0,0 +1,2 @@
+import createReducer from "../helpers/createReducer";
+import * as types from "../actions/types";
