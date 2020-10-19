32c9dab359aa4d7f4236a0217377cecad05e8a5e
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2020-10-19 18:09:59 +0900

    Update App.js

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <ForgotPassword />
+        <LoggedOut />
       </Provider> 
     );
   }
