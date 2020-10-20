da3c4068b4ba2ced5b531664400dc1baf37dc617
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2020-10-20 17:37:22 +0900

    UPDATE TabNavigator.js

--- src/styles/colors/index.js
+++ src/styles/colors/index.js
@@ -1,7 +1,8 @@
 export default {
   black: "#000000",
   white: "#ffffff",
-  green01: "#3D6647",
+  // green01: "#3D6647",
+  green01: "#6B52AE",
   pink01: "#F4F1F1",
   darkOrange: "#C53925",
   lightGrey: "#d8d8d8",
