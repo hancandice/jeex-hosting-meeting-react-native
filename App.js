96db183ae719514d9b2be61fb6abe45dc440ee98
Candice Han <jeeyoung.han82@gmail.com>
2020-11-04 22:43:54 +0900

    Update App.js

--- App.js
+++ App.js
@@ -79,7 +79,7 @@ export default class App extends Component {
       <Provider store={store}>
         <NavigationContainer>
           <RootStack.Navigator
-            initialRouteName="Welcome"
+            initialRouteName="Home"
             mode="modal"
             screenOptions={{
               headerStyle: {
